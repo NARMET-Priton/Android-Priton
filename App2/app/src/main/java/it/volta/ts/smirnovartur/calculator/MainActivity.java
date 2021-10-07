c83ea358d478cc153bc208919295156ab4f16855
WlanKasper <asu.artur.smirnov@gmail.com>
2021-10-07 22:21:53 +0200

    Weaver app 0.1

---
+++ App2/app/src/main/java/it/volta/ts/smirnovartur/calculator/MainActivity.java
@@ -0,0 +1,106 @@
+package it.volta.ts.smirnovartur.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText appUserField;
+    private Button mainBtn;
+    private TextView resultInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        appUserField = findViewById(R.id.appUserField);
+        mainBtn = findViewById(R.id.mainBtn);
+        resultInfo = findViewById(R.id.resultInfo);
+
+        mainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {   // Обработчик события
+                if (appUserField.getText().toString().trim().equals(""))
+                    Toast.makeText(MainActivity.this, R.string.noUserInput, Toast.LENGTH_LONG).show();
+                else{
+                    String city = appUserField.getText().toString();
+                    String key = "a7fea4c398bf4f3d6dad93ba4e78b0de";
+                    String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=metric";
+
+                    new GetURLData().execute(url);
+                }
+            }
+        });
+    }
+
+    private class GetURLData extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            resultInfo.setText("Loading…");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);                                 // Откритые URL соединение
+                connection = (HttpURLConnection) url.openConnection();         //Откритые http соединения
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();              // Считывание потока
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String str = "";
+
+                while((str = reader.readLine()) != null){
+                    buffer.append(str).append("\n");
+                }
+
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if(connection != null)
+                    connection.disconnect();
+
+                try{
+                    if(reader != null)
+                        reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+
+            resultInfo.setText(result);
+        }
+    }
+}
