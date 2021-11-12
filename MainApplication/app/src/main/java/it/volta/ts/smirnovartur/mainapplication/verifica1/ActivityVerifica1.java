ee2f9ea5fa97bc0e89013866d4bb31abcfe1f27f
WlanKasper <asu.artur.smirnov@gmail.com>
2021-11-12 20:39:25 +0100

    onFocusChange 0.1

--- MainApplication/app/src/main/java/it/volta/ts/smirnovartur/mainapplication/verifica1/ActivityVerifica1.java
+++ MainApplication/app/src/main/java/it/volta/ts/smirnovartur/mainapplication/verifica1/ActivityVerifica1.java
@@ -2,51 +2,63 @@ package it.volta.ts.smirnovartur.mainapplication.verifica1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.hardware.biometrics.BiometricManager;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.text.Editable;
-import android.util.Xml;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.xmlpull.v1.XmlPullParser;
-
 import it.volta.ts.smirnovartur.mainapplication.R;
 
-public class ActivityVerifica1 extends AppCompatActivity {
+    public class ActivityVerifica1 extends AppCompatActivity implements View.OnFocusChangeListener {
 
-    Button exitButton;
-    EditText inputEditText1, inputEditText2;
-    TextView middleTextView;
+        Button exitButton;
+        EditText inputEditText1, inputEditText2;
+        TextView middleTextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_verifica1);
+        String strNum_1, strNum_2;
 
-        exitButton = (Button) findViewById(R.id.ExitButton);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_verifica1);
+
+            exitButton     = findViewById(R.id.ExitButton);
+            middleTextView = findViewById(R.id.MiddleTextView);
+            inputEditText1 = findViewById(R.id.InputEditText1);
+            inputEditText2 = findViewById(R.id.InputEditText2);
+
+            inputEditText1.setOnFocusChangeListener(this);
+            inputEditText2.setOnFocusChangeListener(this);
+        }
+
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public void onFocusChange(View view, boolean hasFocus) {
+            if (!hasFocus) {
+                String strOutput = (String) middleTextView.getText();
+                switch (view.getId()) {
+                    case R.id.InputEditText1:
+                        strNum_1 = inputEditText1.getText().toString().replaceAll("[^\\\\d.]","");
 
 
+                        break;
+
+                    case R.id.InputEditText2:
+                        strNum_2 = inputEditText2.getText().toString().replaceAll("[^\\\\d.]","");
+
+                        strOutput = strOutput.replace(" null? ", "23");
+                        middleTextView.setText(strOutput);
+
+                        strOutput = strOutput.replace(" null?? ", "43");
+                        middleTextView.setText(strOutput);
+
+                        strOutput = strOutput.replace(" null??? ", "63");
+                        middleTextView.setText(strOutput);
+
+                        break;
+                }
+            }
+        }
     }
-
-    protected int[] checkNumbers(){
-        inputEditText1 = findViewById(R.id.InputEditText1);
-        inputEditText2 = findViewById(R.id.InputEditText2);
-        middleTextView = findViewById(R.id.MiddleTextView);
-
-        String str1 = inputEditText1.getText().toString().replaceAll("\\s",""),
-               str2 = inputEditText2.getText().toString().replaceAll("\\s","");
-
-        // добавить проверок
-
-        return new int[]{Integer.parseInt(str1), Integer.parseInt(str2)};
-    }
-
-    protected int divideFirstSecond(int num1, int num2){
-        return num1%num2;
-    }
-
-
-}
